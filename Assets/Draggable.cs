f8986b9a487bf31cec921706964726844fd0b6ce
Alexwillbeabletocode <alexsaffignahayes@gmail.com>
2026-06-12 18:22:45 +1000

    Draggable: initialize progress bar to 0 at Start so editor scale isn't used as fill level

--- Assets/Draggable.cs
+++ Assets/Draggable.cs
@@ -37,6 +37,8 @@ public class Draggable : MonoBehaviour
 
         if (progressBar != null)
             barBaseScale = progressBar.localScale;
+
+        UpdateProgressBar();
     }
 
     void Update()
